39db89dea7adf5548cdb7d9b5a844b9271d803ab
unknown <florian.combarel@orange.fr>
2018-10-22 12:41:01 +0200

    Correction du dernier push

--- TestMyPoint.java
+++ TestMyPoint.java
@@ -1,17 +1,36 @@
 package main.fr.ut2j.m1ice.ootesting;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Before;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestMyPoint {
 	
+	@InjectMocks
 	private MyPoint point;
+	
 	private MyPoint point2;
 	private MyPoint point3;
 	private MyPoint point4;
+	
+	@Mock
+	private Random alea1;
+	
+	@Mock
+	private Random alea2;
+	
+	@Mock
+	private ITranslation pointT;
 	
 	@Before
 	public void setUp() {
@@ -82,5 +101,30 @@ public class TestMyPoint {
 		MyPoint test2 = point2.horizontalSymmetry(null);
 		assertEquals(-1.23,test1.getX(),0.001);
 		assertEquals(5.689,test1.getY(),0.001);
+	}
+	
+	@Test
+	public void testGetMiddlePoint() {
+		MyPoint test1 = point.getMiddlePoint(point2);
+		assertEquals(point2.getX()/2d,test1.getX(),0.001);
+		assertEquals(point2.getY()/2d,test1.getY(),0.001);
+	}
+	
+	@Test
+	public void testSetPoint() {
+		when(alea1.nextInt()).thenReturn(5);
+		when(alea2.nextInt()).thenReturn(2);
+		point.setPoint(alea1, alea2);
+		assertEquals(5,point.getX(),0.001);
+		assertEquals(2,point.getY(),0.001);
+	}
+	
+	@Test
+	public void testTranslate() {
+		when(pointT.getTx()).thenReturn(7);
+		when(pointT.getTy()).thenReturn(4);
+		point.translate(pointT);
+		assertEquals(7,point.getX(),0.001);
+		assertEquals(4,point.getY(),0.001);
 	}
 }
